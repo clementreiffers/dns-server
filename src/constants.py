c98a12bf6fc47d84237eb97d64c6d7466116180a
clementreiffers <reiffersclement@gmail.com>
2023-02-25 11:32:01 +0100

    constante pour clock watchdog

--- src/constants.py
+++ src/constants.py
@@ -2,8 +2,9 @@ TMP_DIR = "../tmp/"
 TMP_URL = f"{TMP_DIR}unknown_urls.txt"
 TMP_STATE = f"{TMP_DIR}state.json"
 
-
 LOGIN_MONGO_PATH = "../login/login.json"
 
 GOOGLE_DNS = "8.8.8.8"
 LOCALHOST = "127.0.0.1"
+
+CLOCK = 60 * 15  # 15 minutes
--- src/watchdog.py
+++ src/watchdog.py
@@ -1,16 +1,16 @@
 import time
 
 from change_dns_address import change_dns
-from constants import GOOGLE_DNS, LOCALHOST, TMP_URL
+from constants import GOOGLE_DNS, LOCALHOST, TMP_URL, CLOCK
 from fs import read_file, empty_file, read_state_dns
-from src.mongo import insert_many_url_to_analyze
+from mongo import insert_many_url_to_analyze
 
 
 def launch_watchdog():
     print("Starting watchdog..")
     start = time.time()
     while True:
-        if read_state_dns()["dns_choosen"] == LOCALHOST and time.time() - start > 10:
+        if read_state_dns()["dns_choosen"] == LOCALHOST and time.time() - start > CLOCK:
             start = time.time()
             change_dns(GOOGLE_DNS)
             data = list(filter(lambda d: d != "", read_file(TMP_URL).split("\n")))
